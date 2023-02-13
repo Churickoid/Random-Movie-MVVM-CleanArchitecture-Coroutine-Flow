06774ff1b8adbc8dbdfcb2421dbeb08b0262f67d
arsen <arseniy323@mail.ru>
2023-02-13 22:49:36 +0300

    bottom navigation realisation

--- app/src/main/java/com/example/randommovie/presentation/MainActivity.kt
+++ app/src/main/java/com/example/randommovie/presentation/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.randommovie.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import com.bumptech.glide.Glide
 import com.example.randommovie.R
@@ -9,6 +10,8 @@ import com.example.randommovie.data.MovieInterfaceImpl
 import com.example.randommovie.data.RetrofitMovieApiInterface
 import com.example.randommovie.databinding.ActivityMainBinding
 import com.example.randommovie.domain.entity.SearchFilter
+import com.example.randommovie.presentation.screen.FilterFragment
+import com.example.randommovie.presentation.screen.ListFragment
 import com.example.randommovie.presentation.screen.MovieFragment
 import kotlinx.coroutines.launch
 import okhttp3.OkHttpClient
@@ -31,10 +34,23 @@ class MainActivity : AppCompatActivity() {
                 .add(R.id.fragmentContainer, MovieFragment())
                 .commit()
         }
-
+        binding.bottomNavigation.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.movie -> replaceFragment(MovieFragment())
+                R.id.filter -> replaceFragment(FilterFragment())
+                R.id.list -> replaceFragment(ListFragment())
+                else -> throw Exception("Invalid ID")
+            }
+            return@setOnItemSelectedListener true
+        }
     }
 
-    companion object{
-        const val IMAGE_URL_BASE = "https://kinopoiskapiunofficial.tech"
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager
+            .beginTransaction()
+            .addToBackStack(null)
+            .replace(R.id.fragmentContainer,fragment)
+            .commit()
     }
+
 }
---
+++ app/src/main/java/com/example/randommovie/presentation/screen/ListFragment.kt
@@ -0,0 +1,19 @@
+package com.example.randommovie.presentation.screen
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.randommovie.R
+
+class ListFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_list,container,false)
+    }
+}
